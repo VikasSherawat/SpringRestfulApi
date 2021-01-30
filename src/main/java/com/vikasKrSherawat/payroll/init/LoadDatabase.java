db43c8fe74a77598f0ddb53baca6779379a863b9
Vikas <vkumar4@paypal.com>
2021-01-30 13:31:40 +0800

    added order cancel & complete API

--- src/main/java/com/vikasKrSherawat/payroll/init/LoadDatabase.java
+++ src/main/java/com/vikasKrSherawat/payroll/init/LoadDatabase.java
@@ -1,7 +1,11 @@
 package com.vikasKrSherawat.payroll.init;
 
 import com.vikasKrSherawat.payroll.model.employee.Employee;
+import com.vikasKrSherawat.payroll.model.order.Order;
+import com.vikasKrSherawat.payroll.model.order.Status;
 import com.vikasKrSherawat.payroll.repository.EmployeeRepository;
+import com.vikasKrSherawat.payroll.repository.OrderRepository;
+import org.aspectj.weaver.ast.Or;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
@@ -13,10 +17,12 @@ public class LoadDatabase {
     private static final Logger log = LoggerFactory.getLogger(LoadDatabase.class);
 
     @Bean
-    CommandLineRunner initDatabase(EmployeeRepository repository){
+    CommandLineRunner initDatabase(EmployeeRepository repository, OrderRepository orderRepository){
         return args -> {
             log.info("Preloading " + repository.save(new Employee("Bilbo", "Baggins", "burglar")));
             log.info("Preloading " + repository.save(new Employee("Frodo", "Baggins", "thief")));
+            log.info("Preloading " + orderRepository.save(new Order("MacBook Pro", Status.COMPLETED)));
+            log.info("Preloading " + orderRepository.save(new Order("Iphone", Status.IN_PROGRESS)));
         };
     }
 }
--- src/main/java/com/vikasKrSherawat/payroll/model/order/OrderModelAssembler.java
+++ src/main/java/com/vikasKrSherawat/payroll/model/order/OrderModelAssembler.java
@@ -4,10 +4,12 @@ import com.vikasKrSherawat.payroll.controller.OrderController;
 import org.springframework.hateoas.CollectionModel;
 import org.springframework.hateoas.EntityModel;
 import org.springframework.hateoas.server.RepresentationModelAssembler;
+import org.springframework.stereotype.Component;
 
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
+@Component
 public class OrderModelAssembler implements RepresentationModelAssembler<Order, EntityModel<Order>> {
 
     @Override
